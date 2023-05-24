c93b99676f2f09e8915fb536678ac6a50660dae0
DavalEnzo <davalenzo@gmail.com>
2023-05-24 16:29:01 +0200

    Ajout page de détails

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import FahrenheitToCelsius from "./FahrenheitToCelsius";
 
-function WeatherDisplay ({ ville }) {
+const WeatherDisplay = ({ ville }) => {
     const [data, setData] = useState([])
     
     useEffect(() => {
         getData()
-    })
-    
+    }, [ville]);
+
     const getData = () => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=paris&lang=fr&appid=4961f48689760d647f370de1feca07cc`)
-            .then((response) => {
-                setData(response.data)
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&lang=fr&appid=4961f48689760d647f370de1feca07cc`, {
+        })
+            .then((res) => {
+                setData(res.data)
             })
-            .catch((error) => {
-                console.log(error)
+            .catch((err) => {
+                console.log(err)
             })
     }
+
+    if(!data.main) return (<center>Chargement...</center>)
+
+    return (
+        <div className="container-meteo rounded-border">
+            <h1 style={{marginBottom: "3%"}}>Ville de {ville}</h1>
+            <div>
+                <FahrenheitToCelsius fahrenheit={data.main.temp}/>
+                <p className="center-align"><img src="/weather.svg" alt="Temps" />Temps : {data.weather[0].description}</p>
+                <p className="center-align"><img src="/water-drop.svg" alt="Humidite" />Humidité : {data.main.humidity}%</p>
+                <p className="center-align"><img src="/Pressure.svg" alt="Pression" />Pression : {data.main.pressure} hPa</p>
+                <p className="center-align"><img src="/wind.svg" alt="Vitesse" />Vitesse du vent : {data.wind.speed} m/s</p>
+            </div>
+        </div>
+    )
 
 }
 export default WeatherDisplay
--- src/router/Routeur.js
+++ src/router/Routeur.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoMatch from './NoMatch'
 import Home from "../pages/Home";
-import WeatherDisplay from "../components/WeatherDisplay";
+import MeteoVille from "../pages/MeteoVille";
 
 const Routeur = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/ville/:ville' element={<WeatherDisplay />} />
+                <Route path='/ville/:ville' element={<MeteoVille />} />
                 <Route path='*' element={<NoMatch />} />
             </Routes>
         </BrowserRouter>
