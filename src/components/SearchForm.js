6f6d5d443632e9a06b250e5840a5e143bc8e19fe
unknown <elgrysyoussef78@gmail.com>
2023-05-24 17:03:11 +0200

    add search api and css -youssef

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,58 +1,54 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import "../css/styles.css"
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+import axios from 'axios'
+
 
 const SearchForm = () => {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ecaedecc253a5fc299cbe2b06e20bba5`
-      );
-      setWeatherData(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Entrez la ville"
-        />
-        <button type="submit">recherche</button>
-      </form>
-    
-      {weatherData && (
-  <div>
-    <p>Coordonnées : {weatherData.coord.lon}, {weatherData.coord.lat}</p>
-    <p>Météo : {weatherData.weather[0].main}</p>
-    <p>Description : {weatherData.weather[0].description}</p>
-    <p>Température : {Math.round(weatherData.main.temp - 273.15)}°C</p>
-    <p>Température minimale : {Math.round(weatherData.main.temp_min - 273.15)}°C</p>
-    <p>Température maximale : {Math.round(weatherData.main.temp_max - 273.15)}°C</p>
-    <p>Pression : {weatherData.main.pressure} hPa</p>
-    <p>Humidité : {weatherData.main.humidity}%</p>
-    <p>Visibilité : {weatherData.visibility} m</p>
-    <p>Vitesse du vent : {weatherData.wind.speed} m/s</p>
-    <p>Direction du vent : {weatherData.wind.deg}°</p>
-    <p>Nuages : {weatherData.clouds.all}%</p>
-    <p>Date et heure de la mesure : {new Date(weatherData.dt * 1000).toLocaleString()}</p>
-    <p>Pays : {weatherData.sys.country}</p>
-  </div>
-)}
-
-    </div>
-  );
-};
-
-export default SearchForm;
+    const [city, setCity] = useState('')
+    const [weatherData, setWeatherData] = useState(null)
 
 
+    return (
+        <div>
+            <div className='searchVille'>
+                <input
+                    type='text'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    placeholder='Entrez la ville'
+                    className='search'
+                />
+                <Link to={`ville/${city}`}>
+                    <button type='submit'>recherche</button>
+                </Link>
+            </div>
+
+            {weatherData && (
+                <div>
+                    <p>
+                        Coordonnées : {weatherData.coord.lon}, {weatherData.coord.lat}
+                    </p>
+                    <p>Météo : {weatherData.weather[0].main}</p>
+                    <p>Description : {weatherData.weather[0].description}</p>
+                    <p>Température : {Math.round(weatherData.main.temp - 273.15)}°C</p>
+                    <p>Température minimale : {Math.round(weatherData.main.temp_min - 273.15)}°C</p>
+                    <p>Température maximale : {Math.round(weatherData.main.temp_max - 273.15)}°C</p>
+                    <p>Pression : {weatherData.main.pressure} hPa</p>
+                    <p>Humidité : {weatherData.main.humidity}%</p>
+                    <p>Visibilité : {weatherData.visibility} m</p>
+                    <p>Vitesse du vent : {weatherData.wind.speed} m/s</p>
+                    <p>Direction du vent : {weatherData.wind.deg}°</p>
+                    <p>Nuages : {weatherData.clouds.all}%</p>
+                    <p>
+                        Date et heure de la mesure :{' '}
+                        {new Date(weatherData.dt * 1000).toLocaleString()}
+                    </p>
+                    <p>Pays : {weatherData.sys.country}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default SearchForm
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
-function Home () {
-    return (
+import React from 'react';
+import SearchForm from '../components/SearchForm';
+import CSS from '../css/styles.css';
+
+function Home() {
+  return (
+    <div className='container-box'>
         <div>
-            <h1>Home</h1>
-        </div>
-    )
+      <h1>recherche :</h1>
+    <SearchForm/>
+    </div>
+    </div>
+  );
 }
-export default Home
+
+export default Home;
