f3da1a1c1d9867a6fe0abdd0c13c8e3f36496c25
DavalEnzo <davalenzo@gmail.com>
2023-05-24 17:45:52 +0200

    Suppression résidus

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -20,30 +20,6 @@ const SearchForm = () => {
                     <button type='submit'>recherche</button>
                 </Link>
             </div>
-
-            {weatherData && (
-                <div>
-                    <p>
-                        Coordonnées : {weatherData.coord.lon}, {weatherData.coord.lat}
-                    </p>
-                    <p>Météo : {weatherData.weather[0].main}</p>
-                    <p>Description : {weatherData.weather[0].description}</p>
-                    <p>Température : {Math.round(weatherData.main.temp - 273.15)}°C</p>
-                    <p>Température minimale : {Math.round(weatherData.main.temp_min - 273.15)}°C</p>
-                    <p>Température maximale : {Math.round(weatherData.main.temp_max - 273.15)}°C</p>
-                    <p>Pression : {weatherData.main.pressure} hPa</p>
-                    <p>Humidité : {weatherData.main.humidity}%</p>
-                    <p>Visibilité : {weatherData.visibility} m</p>
-                    <p>Vitesse du vent : {weatherData.wind.speed} m/s</p>
-                    <p>Direction du vent : {weatherData.wind.deg}°</p>
-                    <p>Nuages : {weatherData.clouds.all}%</p>
-                    <p>
-                        Date et heure de la mesure :{' '}
-                        {new Date(weatherData.dt * 1000).toLocaleString()}
-                    </p>
-                    <p>Pays : {weatherData.sys.country}</p>
-                </div>
-            )}
         </div>
     )
 }
